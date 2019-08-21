782c7b23fc4787788772473d2114cc4057df6da3
Kyoichiro Yamada <master@kyoh86.dev>
2019-08-21 16:47:58 +0900

    fix lints by shellcheck

--- plugin/setup.sh
+++ plugin/setup.sh
@@ -1,16 +1,18 @@
+#!/bin/sh
+
 # setup.sh
 # Maintainer:	kyoh86
 # License:		MIT License(http://www.opensource.org/licenses/MIT)
 
-if [[ -n "${VIM_TERMINAL}" ]]; then
-  function vim() {
+if [ -n "${VIM_TERMINAL}" ]; then
+  vim() {
     ${EDITOR} "$@"
   }
-  if [[ "${VIM_EDITERM_DEFDROP}" == "1" ]]; then
-    eval 'function '${VIM_EDITERM_DEFDROP_PREFIX}'drop() { ${VIM_EDITERM_DROP} "$@"; }'
-    eval 'function '${VIM_EDITERM_DEFDROP_PREFIX}'edit() { VIM_EDITERM_OPENER=e ${VIM_EDITERM_DROP} "$@"; }'
-    eval 'function '${VIM_EDITERM_DEFDROP_PREFIX}'new() { VIM_EDITERM_OPENER=n ${VIM_EDITERM_DROP} "$@"; }'
-    eval 'function '${VIM_EDITERM_DEFDROP_PREFIX}'vnew() { VIM_EDITERM_OPENER=v ${VIM_EDITERM_DROP} "$@"; }'
-    eval 'function '${VIM_EDITERM_DEFDROP_PREFIX}'tabnew() { VIM_EDITERM_OPENER=t ${VIM_EDITERM_DROP} "$@"; }'
+  if [ "${VIM_EDITERM_DEFDROP}" = "1" ]; then
+    eval "function ${VIM_EDITERM_DEFDROP_PREFIX}drop()"' { ${VIM_EDITERM_DROP} "$@"; }'
+    eval "function ${VIM_EDITERM_DEFDROP_PREFIX}edit()"' { VIM_EDITERM_OPENER=e ${VIM_EDITERM_DROP} "$@"; }'
+    eval "function ${VIM_EDITERM_DEFDROP_PREFIX}new()"' { VIM_EDITERM_OPENER=n ${VIM_EDITERM_DROP} "$@"; }'
+    eval "function ${VIM_EDITERM_DEFDROP_PREFIX}vnew()"' { VIM_EDITERM_OPENER=v ${VIM_EDITERM_DROP} "$@"; }'
+    eval "function ${VIM_EDITERM_DEFDROP_PREFIX}tabnew()"' { VIM_EDITERM_OPENER=t ${VIM_EDITERM_DROP} "$@"; }'
   fi
 fi
